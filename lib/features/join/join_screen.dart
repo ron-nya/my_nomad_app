48c342a8d8eac726bfc16d42286484136b7d12b2
ron_nya <sohnjw92@naver.com>
2024-07-12 13:43:51 +0900

    unused import 제거

--- lib/features/join/join_screen.dart
+++ lib/features/join/join_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:go_router/go_router.dart';
-import 'package:my_app/config/theme/app_colors.dart';
 import 'package:my_app/constants/gaps.dart';
 import 'package:my_app/constants/sizes.dart';
 import 'package:my_app/widgets/common_padding.dart';
